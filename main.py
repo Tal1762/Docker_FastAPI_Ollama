0f06ec141a57417f5c59fefc214f624b3c113031
Tal1762 <113717843+Tal1762@users.noreply.github.com>
2025-02-27 19:14:46 -0600

    updated commented version

--- main.py
+++ main.py
@@ -70,44 +70,104 @@ async def generate_text(request: PromptRequest):
     return {"generated_text": response}
 
 """
+# Decompiled with PyLingual (https://pylingual.io)
+# Internal filename: /app/main.py
+# Bytecode version: 3.10.0rc2 (3439)
+# Source timestamp: 2025-02-27 23:22:32 UTC (1740698552)
+
+import ollama
+from fastapi import FastAPI, HTTPException, UploadFile, File
+from pydantic import BaseModel
+from typing import List
+#from langchain_community.document_loaders import PyPDFLoader
+#from langchain.text_splitter import RecursiveCharacterTextSplitter
+import tempfile
 import torch
 import io
-from PIL import Image as PIL_Image
 from transformers import MllamaForConditionalGeneration, AutoProcessor, MllamaProcessor, file_utils, AutoModelForVision2Seq
 from pdf2image import convert_from_path
 import base64
 import os
+import replicate
 
-model_id = "HuggingFaceTB/SmolVLM-Instruct"
+os.environ['REPLICATE_API_TOKEN'] = "r8_8Q74pvqI5saxf4Rmz1UoLo6OwxuPwDx3yqK7A"
 
-model = AutoModelForVision2Seq.from_pretrained(model_id, device_map="cuda", torch_dtype=torch.bfloat16, cache_dir=r'D:\Software\llama3cache')
-processor = AutoProcessor.from_pretrained(model_id, cache_dir=r'D:\Software\llama3cache')
+def ask_ai(file: UploadFile=File(...)):
+    model_id = 'HuggingFaceTB/SmolVLM-Instruct'
+    model = AutoModelForVision2Seq.from_pretrained(model_id, torch_dtype=torch.bfloat16)
+    processor = AutoProcessor.from_pretrained(model_id)
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.pdf') as temp_file:
+        temp_file.write(file.file.read())
+        temp_file_path = temp_file.name
+        pil_image_lst = convert_from_path(temp_file_path)
+    pil_image = pil_image_lst[0]
+    conversation = [{'role': 'user', 'content': [{'type': 'image', 'image': pil_image}, {'type': 'text', 'text': 'Describe this image in two sentences of 30 words or less'}]}]
+    print(torch.version.cuda)
+    if torch.cuda.is_available():
+        print('GPU is available. The model will use the GPU.')
+    else:
+        print('GPU is not available. The model will use the CPU.')
+    prompt = processor.apply_chat_template(conversation, add_generation_prompt=True, tokenize=False)
+    print('prompt loaded yessir')
+    inputs = processor(text=prompt, images=pil_image, return_tensors='pt').to(model.device)
+    print('we getting there')
+    output = model.generate(**inputs, temperature=0.7, top_p=0.9, max_new_tokens=25, do_sample=True)
+    print('we got output boys')
+    print('text&image_output: ', processor.decode(output[0])[len(prompt):])
+    return processor.decode(output[0])[len(prompt):]
+  
+app = FastAPI()
 
-pdf_file = r"statement.pdf"
-pil_image_lst = convert_from_path(pdf_file) # This returns a list even for a 1 page pdf
-pil_image = pil_image_lst[0]
+class PromptRequest(BaseModel):
+    model: str = 'llama3.2:1b'
+    prompt: str
+    file: bytes = ''
 
-conversation = [
-    {
-    "role": "user",
-    "content": [
-        {"type": "image", "image": pil_image},
-        {"type": "text", "text": "Describe this image in two sentences of 30 words or less"},
-        ],
-    },
-]
-print(torch.version.cuda)
-display(pil_image)
-if torch.cuda.is_available():
-    print("GPU is available. The model will use the GPU.")
-else:
-    print("GPU is not available. The model will use the CPU.")
+async def generate_response(model: str, prompt: str) -> str:
+    try:
+        stream = ollama.chat(model=model, messages=[{'role': 'user', 'content': prompt}], stream=True)
+        response_text = ''
+        for chunk in stream:
+            response_text += chunk['message']['content']
+        return response_text
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f'Error generating response: {e}')
 
-prompt = processor.apply_chat_template(conversation, add_generation_prompt=True,tokenize=False)
-print("prompt loaded yessir")
-inputs = processor(text = prompt, images = pil_image, return_tensors="pt").to(model.device)
-print("we getting there")
-output = model.generate(**inputs, temperature=0.7, top_p=0.9, max_new_tokens=512, early_stopping=True)
-print("we got output boys")
-print("text&image_output: ",processor.decode(output[0])[len(prompt):])
+def process_pdf(pdf_path):
+    loader = PyPDFLoader(pdf_path)
+    pages = loader.load()
+    document_text = ''.join([page.page_content for page in pages])
+    print([page.page_content for page in pages])
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=300, chunk_overlap=40)
+    chunks = text_splitter.create_documents([document_text])
+    return chunks
+
+@app.post('/ai/')
+async def ask_question(file: UploadFile=File(...)):
+  output = replicate.run(
+    "lucataco/smolvlm-instruct:e79f1e0eb64fe9a145d0a0afd6127d43b37de66eaaa2e00ff3d165bc14097dfb",
+    input={
+        "image": "https://replicate.delivery/pbxt/M41uQ4M8J9FEqxRJ0tNnliJF2PNJIeGjdid66k2uHOLgv5OJ/weather.png",
+        "prompt": "Where do the severe droughts happen according to this image?",
+        "max_new_tokens": 500
+    }
+  )
+  return {'response': output}
+
+@app.post('/file/')
+async def upload_file(file: UploadFile=File(...)):
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.pdf') as temp_file:
+        temp_file.write(file.file.read())
+        temp_file_path = temp_file.name
+    document_chunks = process_pdf(temp_file_path)
+    print(type(file.file.read()))
+    response = await generate_text(PromptRequest(prompt='Find the total net sales in this financial statement, do not respond with anything other than the number:' + document_chunks[0].page_content))
+    return {'filename': file.filename, 'response': response['generated_text']}
+
+@app.post('/generate')
+async def generate_text(request: PromptRequest):
+    model = request.model
+    prompt = request.prompt
+    response = await generate_response(model, prompt)
+    return {'generated_text': response}
 """
